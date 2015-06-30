6c135dc22e81626a91d66a05e6c4b11367f2672b
Ondrej Vancak <ondrej@hot.ee>
2015-06-30 19:56:50 +0200

    Add TodayFragment

--- app/src/main/kotlin/cz/lamorak/kotlinweather/activity/MainActivity.kt
+++ app/src/main/kotlin/cz/lamorak/kotlinweather/activity/MainActivity.kt
@@ -1,23 +1,52 @@
 package cz.lamorak.kotlinweather.activity
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener
+import android.support.v4.app.Fragment
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import cz.lamorak.kotlinweather.R
+import cz.lamorak.kotlinweather.fragment.TodayFragment
 import kotlinx.android.synthetic.activity_main.*
 import kotlin.properties.Delegates
 
-public class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
     val actionBarDrawerToggle: ActionBarDrawerToggle by Delegates.lazy {
         ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super<AppCompatActivity>.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         drawer_layout.setDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+
+        val drawer_toolbar = findViewById(R.id.drawer_toolbar) as Toolbar
+        drawer_toolbar.setNavigationOnClickListener {
+            drawer_layout.closeDrawers()
+        }
+
+        navigation_view.setNavigationItemSelectedListener(this)
+        val menuItem = navigation_view.getMenu().findItem(R.id.navigation_item_1)
+        onNavigationItemSelected(menuItem)
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem?): Boolean {
+        if (menuItem?.isChecked() ?: true) return false
+
+        val fragmentTransaction = getSupportFragmentManager().beginTransaction()
+        when (menuItem?.getItemId()) {
+            R.id.navigation_item_1 -> {
+                fragmentTransaction.replace(R.id.fragment_container, TodayFragment.newInstance()).commit()
+            }
+        }
+
+        menuItem?.setChecked(true)
+        drawer_layout.closeDrawers()
+        return true
     }
 }
--- app/src/main/kotlin/cz/lamorak/kotlinweather/fragment/TodayFragment.kt
+++ app/src/main/kotlin/cz/lamorak/kotlinweather/fragment/TodayFragment.kt
@@ -1,7 +1,29 @@
 package cz.lamorak.kotlinweather.fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cz.lamorak.kotlinweather.R
+import kotlinx.android.synthetic.fragment_today
 
 public class TodayFragment : Fragment() {
-    
+
+    companion object {
+        fun newInstance() : TodayFragment {
+            val fragment = TodayFragment()
+            return fragment
+        }
+    }
+
+
+    override fun onCreate(args: Bundle?) {
+        super.onCreate(args)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(R.layout.fragment_today, container, false)
+        return view
+    }
 }
