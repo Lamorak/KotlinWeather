7763ab27e6bc9ef93180ab7fefd26bacb41f50e1
Ondrej Vancak <ondrej@hot.ee>
2015-07-02 17:49:50 +0200

    Make TodayFragment retained

--- app/src/main/kotlin/cz/lamorak/kotlinweather/activity/MainActivity.kt
+++ app/src/main/kotlin/cz/lamorak/kotlinweather/activity/MainActivity.kt
@@ -38,10 +38,13 @@ public class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListene
     override fun onNavigationItemSelected(menuItem: MenuItem?): Boolean {
         if (menuItem?.isChecked() ?: true) return false
 
-        val fragmentTransaction = getSupportFragmentManager().beginTransaction()
+        val fragmentManaget = getSupportFragmentManager()
         when (menuItem?.getItemId()) {
             R.id.navigation_item_1 -> {
-                fragmentTransaction.replace(R.id.fragment_container, TodayFragment.newInstance()).commit()
+                val fragment = fragmentManaget.findFragmentByTag(TodayFragment.tag()) ?: TodayFragment.newInstance()
+                fragmentManaget.beginTransaction()
+                        .replace(R.id.fragment_container, fragment, TodayFragment.tag())
+                        .commit()
             }
         }
 
--- app/src/main/kotlin/cz/lamorak/kotlinweather/fragment/TodayFragment.kt
+++ app/src/main/kotlin/cz/lamorak/kotlinweather/fragment/TodayFragment.kt
@@ -12,20 +12,24 @@ import com.squareup.okhttp.Request
 import com.squareup.picasso.Picasso
 import cz.lamorak.kotlinweather.R
 import cz.lamorak.kotlinweather.entity.WeatherResponse
-import kotlinx.android.synthetic.viewport_today_header.*
-import kotlinx.android.synthetic.viewport_today_detail.*
+import kotlinx.android.synthetic.viewport_today_header.view.*
+import kotlinx.android.synthetic.viewport_today_detail.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.async
 import org.jetbrains.anko.uiThread
 
 public class TodayFragment : Fragment(), AnkoLogger {
 
-    var weatherResponse = WeatherResponse()
+    var weatherResponse: WeatherResponse? = null
 
     companion object {
         fun newInstance() : TodayFragment {
             val fragment = TodayFragment()
             return fragment
+        }
+
+        fun tag() : String {
+            return "today_fragment"
         }
     }
 
@@ -36,8 +40,13 @@ public class TodayFragment : Fragment(), AnkoLogger {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_today, container, false)
-        return view
+        return inflater?.inflate(R.layout.fragment_today, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super<Fragment>.onViewCreated(view, savedInstanceState)
+        info { "view created" }
+        initView()
     }
 
     fun loadForecast() {
@@ -45,7 +54,7 @@ public class TodayFragment : Fragment(), AnkoLogger {
             val request = Request.Builder().url("http://api.openweathermap.org/data/2.5/weather?q=Prague").build()
             val response = OkHttpClient().newCall(request).execute()
 
-            weatherResponse = Gson().fromJson<WeatherResponse>(response.body().string()) ?: weatherResponse
+            weatherResponse = Gson().fromJson<WeatherResponse>(response.body().string())
             uiThread {
                 initView()
             }
@@ -53,13 +62,15 @@ public class TodayFragment : Fragment(), AnkoLogger {
     }
 
     fun initView() {
-        Picasso.with(getActivity()).load(weatherResponse.icon()).into(conditions_icon)
-        temperature_label.setText(weatherResponse.temperature())
-        conditions_label.setText(weatherResponse.description())
-        detail_clouds.setText(weatherResponse.cloudines())
-        detail_humidity.setText(weatherResponse.humidity())
-        detail_pressure.setText(weatherResponse.pressure())
-        detail_wind.setText(weatherResponse.windSpeed())
-        detail_direction.setText(weatherResponse.windDirection())
+        info { weatherResponse }
+        val view = getView()
+        Picasso.with(getActivity()).load(weatherResponse?.icon()).into(view.conditions_icon)
+        view.temperature_label.setText(weatherResponse?.temperature())
+        view.conditions_label.setText(weatherResponse?.description())
+        view.detail_clouds.setText(weatherResponse?.cloudines())
+        view.detail_humidity.setText(weatherResponse?.humidity())
+        view.detail_pressure.setText(weatherResponse?.pressure())
+        view.detail_wind.setText(weatherResponse?.windSpeed())
+        view.detail_direction.setText(weatherResponse?.windDirection())
     }
 }
